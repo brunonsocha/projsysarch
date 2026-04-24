1a01421b33833bbe15bb11c8321eafd9c348fdba
s-A013-08 <s-A013-08@CAMPUS.UEK.KRAKOW.PL>
2026-04-24 19:18:48 +0200

    fb setup

--- firebase.js
+++ firebase.js
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebasestorage"
+import { collection, getDocs } from "firebase/firebasestorage"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,3 +17,12 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "products"));
+
+querySnapshot.forEach((doc) => {
+  console.log(doc.data());
+  console.log(doc.data()['name'],doc.data()['type'],doc.data()['quantity']);
+});
