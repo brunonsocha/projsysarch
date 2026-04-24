7b0eeb83f7430a42c794a634bc0982c24faa9c54
s-A013-08 <s-A013-08@CAMPUS.UEK.KRAKOW.PL>
2026-04-24 19:01:23 +0200

    script from firebase

---
+++ script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKHxrQMYPp2FfydgFkwVHnssZ4eW5LMaU",
+  authDomain: "prosysmob-5d7d9.firebaseapp.com",
+  projectId: "prosysmob-5d7d9",
+  storageBucket: "prosysmob-5d7d9.firebasestorage.app",
+  messagingSenderId: "303969123495",
+  appId: "1:303969123495:web:fb56de75cdd558ac742f82"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
